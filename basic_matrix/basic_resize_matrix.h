1b147f5f62a221ba838c66f439c13f2cee5016b9
wangchao <chao.wang@56qq.com>
2017-09-21 16:59:38 +0800

    格式化一下代码

--- basic_matrix/basic_resize_matrix.h
+++ basic_matrix/basic_resize_matrix.h
@@ -127,103 +127,168 @@ private:
 public:
     // range iterator
     friend class Basic_Matrix_Index_Iterator<Basic_Resize_Matrix, value_type>;
-    typedef Basic_Matrix_Index_Iterator<Basic_Resize_Matrix, value_type> iterator;
-
     friend class Basic_Matrix_Index_Iterator<const Basic_Resize_Matrix, const value_type>;
+
+    typedef Basic_Matrix_Index_Iterator<Basic_Resize_Matrix, value_type>             iterator;
     typedef Basic_Matrix_Index_Iterator<const Basic_Resize_Matrix, const value_type> const_iterator;
+    typedef std::reverse_iterator<iterator>                                          reverse_iterator;
+    typedef std::reverse_iterator<const_iterator>                                    const_reverse_iterator;
 
-    typedef std::reverse_iterator<iterator>            reverse_iterator;
-    typedef std::reverse_iterator<const_iterator>      const_reverse_iterator;
+    iterator
+    begin()
+    { return {this, 0}; }
 
-    iterator begin() { return {this, 0}; }
-    iterator end() { return {this, row_size()*col_size()}; }
+    iterator
+    end()
+    { return {this, row_size()*col_size()}; }
 
-    const_iterator begin() const { return {this, 0}; }
-    const_iterator end() const { return {this, row_size()*col_size()}; }
+    const_iterator
+    begin() const
+    { return {this, 0}; }
 
-    reverse_iterator rbegin() { return reverse_iterator(end()); }
-    reverse_iterator rend() { return reverse_iterator(begin()); }
+    const_iterator
+    end() const
+    { return {this, row_size()*col_size()}; }
 
-    const_reverse_iterator rbegin() const { return const_reverse_iterator(end()); }
-    const_reverse_iterator rend() const { return const_reverse_iterator(begin()); }
+    reverse_iterator
+    rbegin()
+    { return reverse_iterator(end()); }
 
-    IteratorRange<iterator> range() { return {begin(), end()}; }
-    IteratorRange<const_iterator> range() const { return {begin(), end()}; }
+    reverse_iterator
+    rend()
+    { return reverse_iterator(begin()); }
 
-    IteratorRange<reverse_iterator> reverse_range() { return {rbegin(), rend()}; }
-    IteratorRange<const_reverse_iterator> reverse_range() const { return {rbegin(), rend()}; }
+    const_reverse_iterator
+    rbegin() const
+    { return const_reverse_iterator(end()); }
+
+    const_reverse_iterator
+    rend() const
+    { return const_reverse_iterator(begin()); }
+
+    IteratorRange<iterator>
+    range()
+    { return {begin(), end()}; }
+
+    IteratorRange<const_iterator>
+    range() const
+    { return {begin(), end()}; }
+
+    IteratorRange<reverse_iterator>
+    reverse_range()
+    { return {rbegin(), rend()}; }
+
+    IteratorRange<const_reverse_iterator>
+    reverse_range() const
+    { return {rbegin(), rend()}; }
 
     // row iterator, same as range iterator
-    typedef iterator row_iterator;
-    typedef const_iterator const_row_iterator;
-    typedef reverse_iterator row_reverse_iterator;
-    typedef const_reverse_iterator const_row_reverse_iterator;
+    typedef iterator                row_iterator;
+    typedef const_iterator          const_row_iterator;
+    typedef reverse_iterator        row_reverse_iterator;
+    typedef const_reverse_iterator  const_row_reverse_iterator;
 
-    row_iterator row_begin(size_type row) { return {this, row*col_size()}; }
-    row_iterator row_end(size_type row) { return {this, (row+1)*col_size()}; }
+    row_iterator
+    row_begin(size_type row)
+    { return {this, row*col_size()}; }
 
-    const_row_iterator row_begin(size_type row) const { return {this, row*col_size()}; }
-    const_row_iterator row_end(size_type row) const { return {this, (row+1)*col_size()}; }
+    row_iterator
+    row_end(size_type row)
+    { return {this, (row+1)*col_size()}; }
 
-    IteratorRange<row_iterator> row_range(size_type row)
-    {
-        assert(row<row_size());
-        return {row_begin(row), row_end(row)};
-    }
+    const_row_iterator
+    row_begin(size_type row) const
+    { return {this, row*col_size()}; }
 
-    IteratorRange<const_row_iterator> row_range(size_type row) const
-    {
-        assert(row<row_size());
-        return {row_begin(row), row_end(row)};
-    }
+    const_row_iterator
+    row_end(size_type row) const
+    { return {this, (row+1)*col_size()}; }
 
-    IteratorRange<row_reverse_iterator> row_reverse_range(size_type row)
-    {
-        assert(row<row_size());
-        return {row_reverse_iterator(row_end(row)), row_reverse_iterator(row_begin(row))};
-    }
+    row_reverse_iterator
+    row_rbegin(size_type row)
+    { return row_reverse_iterator(row_end(row)); }
 
-    IteratorRange<const_row_reverse_iterator> row_reverse_range(size_type row) const
-    {
-        assert(row<row_size());
-        return {const_row_reverse_iterator(row_end(row)), const_row_reverse_iterator(row_begin(row))};
-    }
+    row_reverse_iterator
+    row_rend(size_type row)
+    { return row_reverse_iterator(row_begin(row)); }
+
+    const_row_reverse_iterator
+    row_rbegin(size_type row) const
+    { return const_row_reverse_iterator(row_end(row)); }
+
+    const_row_reverse_iterator
+    row_rend(size_type row) const
+    { return const_row_reverse_iterator(row_begin(row)); }
+
+    IteratorRange<row_iterator>
+    row_range(size_type row)
+    { return {row_begin(row), row_end(row)}; }
+
+    IteratorRange<const_row_iterator>
+    row_range(size_type row) const
+    { return {row_begin(row), row_end(row)}; }
+
+    IteratorRange<row_reverse_iterator>
+    row_reverse_range(size_type row)
+    { return {row_rbegin(row), row_rend(row)}; }
+
+    IteratorRange<const_row_reverse_iterator>
+    row_reverse_range(size_type row) const
+    { return {row_rbegin(row), row_rend(row)}; }
 
     // col iterator
-    typedef Basic_Matrix_Col_Index_Iterator<Basic_Resize_Matrix, value_type> col_iterator;
-    typedef Basic_Matrix_Col_Index_Iterator<const Basic_Resize_Matrix, const value_type> const_col_iterator;
-    typedef std::reverse_iterator<col_iterator>            col_reverse_iterator;
-    typedef std::reverse_iterator<const_col_iterator>      const_col_reverse_iterator;
+    typedef Basic_Matrix_Col_Index_Iterator<Basic_Resize_Matrix, value_type>                col_iterator;
+    typedef Basic_Matrix_Col_Index_Iterator<const Basic_Resize_Matrix, const value_type>    const_col_iterator;
+    typedef std::reverse_iterator<col_iterator>                                             col_reverse_iterator;
+    typedef std::reverse_iterator<const_col_iterator>                                       const_col_reverse_iterator;
 
-    col_iterator col_begin(size_type col) { return {this, col}; }
-    col_iterator col_end(size_type col) { return {this, row_size()*col_size()+col}; }
+    col_iterator
+    col_begin(size_type col)
+    { return {this, col}; }
 
-    const_col_iterator col_begin(size_type col) const { return {this, col}; }
-    const_col_iterator col_end(size_type col) const { return {this, row_size()*col_size()+col}; }
+    col_iterator
+    col_end(size_type col)
+    { return {this, row_size()*col_size()+col}; }
 
-    IteratorRange<col_iterator> col_range(size_type col)
-    {
-        assert(col<col_size());
-        return {col_begin(col), col_end(col)};
-    }
+    const_col_iterator
+    col_begin(size_type col) const
+    { return {this, col}; }
 
-    IteratorRange<const_col_iterator> col_range(size_type col) const
-    {
-        assert(col<col_size());
-        return {col_begin(col), col_end(col)};
-    }
+    const_col_iterator
+    col_end(size_type col) const
+    { return {this, row_size()*col_size()+col}; }
 
-    IteratorRange<col_reverse_iterator> col_reverse_range(size_type col)
-    {
-        assert(col<col_size());
-        return {col_reverse_iterator(col_end(col)), col_reverse_iterator(col_begin(col))};
-    }
+    col_reverse_iterator
+    col_rbegin(size_type col)
+    { return col_reverse_iterator(col_end(col)); }
 
-    IteratorRange<const_col_reverse_iterator> col_reverse_range(size_type col) const
-    {
-        assert(col<col_size());
-        return {const_col_reverse_iterator(col_end(col)), const_col_reverse_iterator(col_begin(col))};
-    }
+    col_reverse_iterator
+    col_rend(size_type col)
+    { return col_reverse_iterator(col_begin(col)); }
+
+    const_col_reverse_iterator
+    col_rbegin(size_type col) const
+    { return const_col_reverse_iterator(col_end(col)); }
+
+    const_col_reverse_iterator
+    col_rend(size_type col) const
+    { return const_col_reverse_iterator(col_begin(col)); }
+
+    IteratorRange<col_iterator>
+    col_range(size_type col)
+    { return {col_begin(col), col_end(col)}; }
+
+    IteratorRange<const_col_iterator>
+    col_range(size_type col) const
+    { return {col_begin(col), col_end(col)}; }
+
+    IteratorRange<col_reverse_iterator>
+    col_reverse_range(size_type col)
+    { return {col_rbegin(col), col_rend(col)}; }
+
+    IteratorRange<const_col_reverse_iterator>
+    col_reverse_range(size_type col) const
+    { return {col_rbegin(col), col_rend(col)}; }
 };
 
 template<typename T, typename S>
